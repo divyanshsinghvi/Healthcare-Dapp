3fc4742b759787a6bd0e6e05075019fd98ad2c4e
Mrinaal Dogra <mrinaald@iitk.ac.in>
2019-04-13 21:54:09 +0530

    Modification in Report structure, and implementation of the corresponding update and getter functions

--- HealthCareFrontend/contracts/AppInterface.sol
+++ HealthCareFrontend/contracts/AppInterface.sol
@@ -9,11 +9,12 @@ contract AppInterface {
 
   mapping (uint => Person) person;
   mapping (address => bool) isRegistered;
-  
+  mapping (address => uint) personToUID;
+
   uint numPersons = 0;
 
 
-  constructor() public {
+  constructor () public {
     admin = msg.sender;
   }
 
@@ -24,17 +25,45 @@ contract AppInterface {
     _;
   }
 
-  function registerPerson(bool _isDoctor) public {
+  function registerPerson (bool _isDoctor) public {
     numPersons++;
     person[numPersons] = new Person(msg.sender, numPersons, _isDoctor);
+    personToUID[msg.sender] = numPersons;
     isRegistered[msg.sender] = true;
 
     emit personRegistered(address(person[numPersons]), numPersons);
   }
 
   function isPersonRegistered () public view returns(bool) {
-    emit personRegistered(address(person[numPersons]), numPersons);
     return isRegistered[msg.sender];
   }
-  
+
+  function getLatestReport () public view returns(string memory, string memory, string memory) {
+    /* Person personContract = person[personToUID[msg.sender]]; */
+    return person[personToUID[msg.sender]].getLatestReport();
+  }
+
+  // TODO: Have to implement the below mentioned function somehow
+  /* function getAllReports () public view returns(string[] memory, string[] memory, string[] memory) { */
+  /*   uint numReports = person[personToUID[msg.sender]].getNumReports(); */
+
+  /*   string[] memory report = new string[](3); */
+  /*   string[] memory vitals = new string[](numReports); */
+  /*   string[] memory prescriptions = new string[](numReports); */
+  /*   string[] memory symptoms = new string[](numReports); */
+
+  /*   for (uint i=1; i<=numReports; ++i){ */
+  /*     report = person[personToUID[msg.sender]].getReportByID(i); */
+  /*     vitals[i] = report[0]; */
+  /*     prescriptions[i] = report[1]; */
+  /*     symptoms[i] = report[2]; */
+  /*   } */
+
+  /*   return (vitals, prescriptions, symptoms); */
+  /* } */
+
+  function updateReportWithUID(uint reportID, string memory vitals, string memory prescriptions, string memory symptoms) public {
+    person[personToUID[msg.sender]].updateReportWithUID(reportID, vitals, prescriptions, symptoms);
+  }
+
 }
--- HealthCareFrontend/contracts/Person.sol
+++ HealthCareFrontend/contracts/Person.sol
@@ -4,10 +4,17 @@ contract Person {
 
   address myAddr;
   uint myUID;
+  string name;
 
   struct Report {
-    mapping (string => string) vitals;
-    mapping (string => string) prescriptions;
+    // Mrinaal: For simplicity, keeping everything as string for now
+    /* mapping (uint => string) vitals; */
+    /* mapping (uint => string) prescriptions; */
+    /* uint numVitals; */
+    /* uint numPrescriptions; */
+    string vitals;
+    string prescriptions;
+
     string symptoms;
   }
 
@@ -26,9 +33,55 @@ contract Person {
   // Data members required for a Doctor
   bool isDoctor = false;
 
-  constructor(address _addr, uint _uid, bool _isDoctor) public {
+  constructor (address _addr, uint _uid, bool _isDoctor) public {
     myAddr = _addr;
     myUID = _uid;
     isDoctor = _isDoctor;
   }
+
+  function getLatestReport () public view returns(string memory, string memory, string memory) {
+    /* Report memory latestReport = myReports[numReports]; */
+    /* uint i; */
+    /* vitals = ""; */
+    /* prescriptions = ""; */
+
+    /* for(i=1; i<=myReports[numReports].numVitals; ++i){ */
+    /*   /\* vitals += myReports[numReports].vitals[i] + "; "; *\/ */
+    /*   // TODO: Have to write concatenate function for string; */
+    /* } */
+
+    /* for(i=1; i<=myReports[numReports].numPrescriptions; ++i){ */
+    /*   prescriptions += myReports[numReports].prescriptions[i]; */
+    /* } */
+
+    /* return (vitals, prescriptions, myReports[numReports].symptoms); */
+    return (myReports[numReports].vitals, myReports[numReports].prescriptions, myReports[numReports].symptoms);
+  }
+
+  function getReportByID (uint reportID) public view returns(string memory, string memory, string memory) {
+    require(reportID>=1 && reportID<=numReports, "The report with the given Report ID does not exist.");
+
+    return (myReports[reportID].vitals, myReports[reportID].prescriptions, myReports[reportID].symptoms);
+  }
+
+  function getNumReports () public view returns(uint) {
+    return numReports;
+  }
+
+  function getName () public view returns(string memory) {
+    return name;
+  }
+
+  function getUID () public view returns(uint) {
+    return myUID;
+  }
+
+  function updateReportWithUID(uint reportID, string memory vitals, string memory prescriptions, string memory symptoms) public {
+    require(reportID>=1 && reportID<=numReports, "The report with the given Report ID does not exist.");
+
+    myReports[reportID].vitals = vitals;
+    myReports[reportID].prescriptions = prescriptions;
+    myReports[reportID].symptoms = symptoms;
+  }
+
 }
