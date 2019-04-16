5a5ba277760b0df058e3aa844b8fe295bbf28a39
Divyansh Singhvi <divyanshsinghvi@gmail.com>
2019-04-16 11:39:28 +0530

    migrations file updated

--- HealthCareFrontend/migrations/2_initial_migration.js
+++ HealthCareFrontend/migrations/2_initial_migration.js
@@ -3,11 +3,10 @@ var HealthReportInterface = artifacts.require("./HealthReportInterface.sol");
 var PersonInterface = artifacts.require("./PersonInterface.sol");
 
 module.exports = function(deployer) {
-     deployer.deploy(HealthReportInterface).then(function() {
-          deployer.deploy(PersonInterface).then(function() {
+     deployer.deploy(HealthReportInterface).then(function() {   
+       return deployer.deploy(PersonInterface).then(function() {
              return deployer.deploy(AppInterface, PersonInterface.address);
          });
      });
-    
     // deployer.deploy(HealthContractInterface);
 };
